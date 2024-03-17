20f76321425e7307bd6396dda3e1c7a057bf514e
lvdengming <lvdengming@foxmail.com>
2024-03-17 18:04:32 +0800

    build: 完善项目配置

--- src/others/date.ts
+++ src/others/date.ts
@@ -1,5 +1,7 @@
 // 获取 2024-01-01 形式字符串
-export function getFormatDateString(date: Date): string {
+function getFormatDateString(date: Date): string {
     const target = date ?? new Date();
     return target.toISOString().slice(0, 10);
 }
+
+console.log(getFormatDateString(new Date()));
