773608465d460dd8ec843b8938bbad54b76f279e
lvdengming <lvdengming@foxmail.com>
2024-04-19 00:41:32 +0800

    feat: 添加中介者模式

---
+++ src/design-patterns/behavioral/mediator.ts
@@ -0,0 +1,80 @@
+// 中介模式【行为型模式】
+// 记忆案例：对话框中的子元素不需要直接相互通信，而是直接通过对话框来通信
+// 章节：https://refactoringguru.cn/design-patterns/mediator
+// 代码：https://refactoringguru.cn/design-patterns/mediator/typescript/example
+
+interface Mediator {
+    notify(sender: object, event: string): void;
+}
+
+class ConcreteMediator implements Mediator {
+    private component1: Component1;
+    private component2: Component2;
+
+    constructor(component1: Component1, component2: Component2) {
+        this.component1 = component1;
+        this.component1.setMediator(this);
+        this.component2 = component2;
+        this.component2.setMediator(this);
+    }
+
+    public notify(sender: object, event: string): void {
+        if (event === 'A') {
+            console.log('Mediator reacts on A and triggers following operations:');
+            this.component2.doC();
+        }
+
+        if (event === 'D') {
+            console.log('Mediator reacts on D and triggers following operations:');
+            this.component1.doB();
+            this.component2.doC();
+        }
+    }
+}
+
+class BaseComponent {
+    protected mediator: Mediator;
+
+    constructor(mediator: Mediator = null) {
+        this.mediator = mediator;
+    }
+
+    public setMediator(mediator: Mediator): void {
+        this.mediator = mediator;
+    }
+}
+
+class Component1 extends BaseComponent {
+    public doA(): void {
+        console.log('Component 1 does A.');
+        this.mediator.notify(this, 'A');
+    }
+
+    public doB(): void {
+        console.log('Component 1 does B.');
+        this.mediator.notify(this, 'B');
+    }
+}
+
+class Component2 extends BaseComponent {
+    public doC(): void {
+        console.log('Component 2 does C.');
+        this.mediator.notify(this, 'C');
+    }
+
+    public doD(): void {
+        console.log('Component 2 does D.');
+        this.mediator.notify(this, 'D');
+    }
+}
+
+const component1 = new Component1();
+const component2 = new Component2();
+const mediator = new ConcreteMediator(component1, component2);
+
+console.log('Client triggers operation A.');
+component1.doA();
+
+console.log('');
+console.log('Client triggers operation D.');
+component2.doD();
