3b5cfe699f04c681f2e7b686444079e3eacde564
lvdengming <lvdengming@foxmail.com>
2024-12-01 11:19:23 +0800

    feat: 移动手写内容至博客

--- src/manual/promise.ts
+++
@@ -1,3 +0,0 @@
-// 手写 Promise 完整案例
-// 参考：https://juejin.cn/post/6850037281206566919
-// todo
--- src/manual/xhr-request.ts
+++
@@ -1,8 +0,0 @@
-// 使用 XMLHttpRequest 进行网络请求
-
-const xhr = new XMLHttpRequest();
-xhr.addEventListener('load', data => {
-    console.log(data);
-});
-xhr.open('GET', 'http://localhost:3000');
-xhr.send();
